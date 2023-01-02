dc078ddac535bcf01d5d2adc76283d6afbce8402
bantealem <bantem2021@gmail.com>
2023-01-02 21:27:59 +0300

    Add Authntication

--- Gemfile
+++ Gemfile
@@ -71,3 +71,7 @@ group :test do
   gem 'selenium-webdriver'
   gem 'webdrivers'
 end
+
+gem 'devise'
+
+gem 'letter_opener'
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,18 @@
 Rails.application.routes.draw do
+  get 'foods/index'
+  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "foods#index"
+
+
+  # resources :users
+  resources :recipes, only: [:index, :show, :new, :create, :destroy]
+
+  resources :foods, only: [:index, :show, :new, :create, :destroy]
+
+
+
+
 end
