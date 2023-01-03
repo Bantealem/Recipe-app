62e3f6bce359fc340d5f42549b2b6814c90b449f
melashu <meshu102@outlook.com>
2023-01-03 08:31:02 +0300

    Generate devise custom controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   get 'foods/index'
-  devise_for :users
+  devise_for :users, controllers: {
+        sessions: 'users/sessions',
+        registrations: 'users/registrations',
+        
+      }
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
