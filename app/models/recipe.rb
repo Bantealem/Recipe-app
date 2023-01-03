9bba4ac9089fba40b1fbc4c809189daa1bc3eaca
bantealem <bantem2021@gmail.com>
2023-01-03 15:09:18 +0300

    Fix Linters Error

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,12 +2,9 @@ class Recipe < ApplicationRecord
   belongs_to :user
   has_many :recipe_foods
   has_many :foods, through: :recipe_foods
-   validates :name, presence: true, length: { maximum: 255 }
-   validates :description, presence: true, length: { maximum: 500 }
-   validates :preparation_time, presence: true
-   validates :cooking_time, presence: true
-   validates :public, presence: true
-
-
-
+  validates :name, presence: true, length: { maximum: 255 }
+  validates :description, presence: true, length: { maximum: 500 }
+  validates :preparation_time, presence: true
+  validates :cooking_time, presence: true
+  validates :public, presence: true
 end
