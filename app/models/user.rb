b80023616c621ab4c805cece11030c7d53610606
melashu <meshu102@outlook.com>
2023-01-03 08:29:56 +0300

    Add model validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable, :confirmable
     has_many :foods, dependent: :destroy
     has_many :recipes, dependent: :destroy
+    validates :name, presence: true, length: { maximum: 255 }
 
 end
