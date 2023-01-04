f640de3690d82883afa60c566ce35013132a1395
bantealem <bantem2021@gmail.com>
2023-01-04 20:01:49 +0300

    Fix Linters Erorr

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,20 +1,21 @@
-  class FoodsController < ApplicationController
-    before_action :authenticate_user!
+class FoodsController < ApplicationController
+  before_action :authenticate_user!
   def index
     @foods = Food.all.where(user_id: current_user.id).order(created_at: :desc)
   end
-  def def new
+
+  def def(_new)
     @new_food = Food.new
   end
-  
-  
+
   def create
-    @new_food = Food.new(user: current_user, name: params[:name], measurement_unit: params[:measurement_unit], price: params[:price])
+    @new_food = Food.new(user: current_user, name: params[:name], measurement_unit: params[:measurement_unit],
+                         price: params[:price])
     if @new_food.save
-      flash.now[:notice] = "Successfully created food."
+      flash.now[:notice] = 'Successfully created food.'
       redirect_to foods_path
     else
-      flash.now[:alert] = "Error creating food."
+      flash.now[:alert] = 'Error creating food.'
       render :new
     end
   end
@@ -23,12 +24,11 @@
     @foods = Food.where(user_id: current_user.id).order(created_at: :desc)
     @food_delete = Food.find(params[:id])
     if @food_delete.destroy
-      flash.now[:notice] = "Successfully deleted food."
+      flash.now[:notice] = 'Successfully deleted food.'
       redirect_to foods_path
     else
-      flash.now[:alert] = "Error deleting food."
+      flash.now[:alert] = 'Error deleting food.'
       render :new
     end
   end
-
 end
