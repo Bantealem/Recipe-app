b714d024216b3ee88629bf631455ab35c06a6931
melashu <meshu102@outlook.com>
2023-01-04 20:37:09 +0300

    Add linter fix

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,2 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+Recipe.create!(name:'cooking', preparation_time: 1, cooking_time: 1,description:'show', user_id: User.last.id, public: true) ;
+Food.create!(name:'Apple',measurement_unit:'g',price: 15.5,quantity: 5, user_id: User.last.id)
