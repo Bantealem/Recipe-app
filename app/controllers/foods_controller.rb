4e95759e082b8d4d2b2140a42143878f1879e117
bantealem <bantem2021@gmail.com>
2023-01-04 19:38:10 +0300

    Add food

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,3 +1,34 @@
-class FoodsController < ApplicationController
-  def index; end
+  class FoodsController < ApplicationController
+    before_action :authenticate_user!
+  def index
+    @foods = Food.all.where(user_id: current_user.id).order(created_at: :desc)
+  end
+  def def new
+    @new_food = Food.new
+  end
+  
+  
+  def create
+    @new_food = Food.new(user: current_user, name: params[:name], measurement_unit: params[:measurement_unit], price: params[:price])
+    if @new_food.save
+      flash.now[:notice] = "Successfully created food."
+      redirect_to foods_path
+    else
+      flash.now[:alert] = "Error creating food."
+      render :new
+    end
+  end
+
+  def destroy
+    @foods = Food.where(user_id: current_user.id).order(created_at: :desc)
+    @food_delete = Food.find(params[:id])
+    if @food_delete.destroy
+      flash.now[:notice] = "Successfully deleted food."
+      redirect_to foods_path
+    else
+      flash.now[:alert] = "Error deleting food."
+      render :new
+    end
+  end
+
 end
--- app/models/food.rb
+++ app/models/food.rb
@@ -5,5 +5,5 @@ class Food < ApplicationRecord
   validates :name, presence: true, length: { maximum: 255 }
   validates :measurement_unit, presence: true, length: { maximum: 100 }
   validates :price, presence: true, numericality: { greater_than_or_equal_to: 0 }
-  validates :quantity, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  # validates :quantity, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 end
